b0b4d96cff5b3142ed5488a77f16c26a8ae658b5
Simone Sestito <simone.dev.help@gmail.com>
2019-07-29 19:39:36 +0200

    Add other app screenshots

--- src/js/sections/projects.js
+++ src/js/sections/projects.js
@@ -9,12 +9,14 @@ import TweenLite from "gsap/TweenLite";
 
 // Fixed smartphone frame inside projects section
 const section = document.getElementById('projects');
-const phoneFrame = section.getElementsByClassName('phone-container')[0];
-const firstAppScreenshot = phoneFrame.getElementsByClassName('app-screenshot')[0];
+const sectionContent = section.getElementsByClassName('section-content')[0];
+const phoneFrame = sectionContent.getElementsByClassName('phone-container')[0];
+const appScreenshots = phoneFrame.getElementsByClassName('app-screenshot');
+const appTexts = sectionContent.getElementsByClassName('phone-app');
 
 new ScrollMagic.Scene({
-        triggerElement: section,
-        triggerHook: 0,
+        triggerElement: phoneFrame,
+        triggerHook: .15,
         duration: () => section.offsetHeight - vh(100)
     })
     .setPin(phoneFrame, {
@@ -22,10 +24,20 @@ new ScrollMagic.Scene({
     })
     .addTo(scrollMagicController);
 
-// When the display fully enters the screen...
-new ScrollMagic.Scene({
-        triggerElement: phoneFrame,
-        triggerHook: .1
-    })
-    .setTween(TweenLite.fromTo(firstAppScreenshot, .5, { opacity: 0 }, { opacity: 1 }))
-    .addTo(scrollMagicController);
+// Change app screenshot based on current scroll position
+for (let i = 0; i < appTexts.length; i++) {
+    const appText = appTexts[i];
+
+    new ScrollMagic.Scene({
+            triggerElement: appText,
+            triggerHook: 0.5,
+            duration: () => appText.offsetHeight
+        })
+        .on("enter", () => {
+            TweenLite.to(appScreenshots[i], .3, { opacity: 1 });
+        })
+        .on("leave", () => {
+            TweenLite.to(appScreenshots[i], .3, { opacity: 0 });
+        })
+        .addTo(scrollMagicController);
+}
