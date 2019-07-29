fbc8e9cfc298fdebe3eec9b1bbb537f72e26c82b
Simone Sestito <simone.dev.help@gmail.com>
2019-07-29 09:25:36 +0200

    Add scroll snap effect on card

--- src/js/sections/card.js
+++ src/js/sections/card.js
@@ -18,10 +18,31 @@ const timeline = new TimelineLite()
     .to("#main", 2, { opacity: 0 }, 0.8)
     .to(".content-card", 1, { boxShadow: "0px 0px 0px 0px" }, 1.8);
 
+let lastScrollProgress = null,
+    lastScrollTimeout = null;
+
 new ScrollMagic.Scene({
         triggerElement: "#card-arrow",
         triggerHook: 0.8,
         duration: () => window.innerHeight - document.getElementById("card-arrow").clientHeight
     })
     .setTween(timeline)
+    .on("progress", e => {
+        const { progress, scrollDirection } = e;
+        console.log({ progress, scrollDirection });
+
+        if (lastScrollTimeout !== null) {
+            clearTimeout(lastScrollTimeout);
+        }
+
+        if (progress > 0.1 && progress < 1 && scrollDirection === "FORWARD") {
+            // Snap scroll to the end after a pause timeout
+            lastScrollProgress = progress;
+            lastScrollTimeout = setTimeout(() => {
+                if (progress === lastScrollProgress) {
+                    TweenLite.to(window, 0.3, { scrollTo: "#content" });
+                }
+            }, 200);
+        }
+    })
     .addTo(scrollMagicController);
