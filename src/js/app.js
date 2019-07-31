aef526feb3b9d9c775b42b8885e2f29726555775
Simone Sestito <simone.dev.help@gmail.com>
2019-07-31 10:23:34 +0200

    Use both PNG and WebP according to the browser

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,9 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js');
 }
 
+import supportsWebP from 'supports-webp';
+supportsWebP
+    .then(supported => supported ? 'webp' : 'no-webp')
+    .then(clazz => document.body.classList.add(clazz));
+
 import './sections';
