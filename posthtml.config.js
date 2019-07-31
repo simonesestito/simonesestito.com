4aa6e599b5067fc97424963b749b6821a68c7f94
Simone Sestito <simone.dev.help@gmail.com>
2019-07-31 10:35:01 +0200

    Support both PNG and WebP in HTML

--- posthtml.config.js
+++ posthtml.config.js
@@ -2,6 +2,9 @@ module.exports = {
     plugins: [
         require('posthtml-svg-inline')({
             root: 'src',
+        }),
+        require('posthtml-webp')({
+            replaceExtension: true,
         })
     ]
 }
