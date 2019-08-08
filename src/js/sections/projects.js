214e7d00db4f78786b9a22e597835012105e5d74
Simone Sestito <simone.dev.help@gmail.com>
2019-08-08 13:38:52 +0200

    Fix Edge IntersectionObserver
    Edge triggers the observer callback at 0 and 1 ratios too.
    Ignore ratios very different from specified treshold.

--- src/js/sections/projects.js
+++ src/js/sections/projects.js
@@ -8,8 +8,15 @@ const appScreenshots = [...section.getElementsByClassName('app-screenshot')];
 const appInfos = section.querySelectorAll('.phone-apps .phone-app');
 
 // Change app screenshot based on current scroll position
+const OBSERVER_THRESHOLD = 0.5;
 const appInfosObserver = new IntersectionObserver(entries => {
     for (const entry of entries) {
+        // Edge triggers the observer callback at 0 and 1 ratios too
+        // Ignore ratios very different from specified treshold
+        if (Math.abs(entry.intersectionRatio - OBSERVER_THRESHOLD) > OBSERVER_THRESHOLD / 2.0) {
+            continue;
+        }
+
         const entryScreenshotId = entry.target.getAttribute('data-app');
         const entryScreenshot = appScreenshots.find(s => s.id === entryScreenshotId);
         if (!entryScreenshot) {
@@ -23,6 +30,6 @@ const appInfosObserver = new IntersectionObserver(entries => {
             entryScreenshot.classList.remove('visible');
         }
     }
-}, { threshold: 0.5 });
+}, { threshold: OBSERVER_THRESHOLD });
 
 appInfos.forEach(e => appInfosObserver.observe(e));
