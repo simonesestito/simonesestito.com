e718bd193849b34d229365f19a46b76ab03e8970
Simone Sestito <simone@simonesestito.com>
2019-08-27 10:31:51 +0200

    Projects: new algorithm to replace shown screenshot

--- src/js/sections/projects.js
+++ src/js/sections/projects.js
@@ -13,7 +13,6 @@ const appInfosObserver = new IntersectionObserver(entries => {
     for (const entry of entries) {
         // Edge triggers the observer callback at 0 and 1 ratios too
         // Ignore ratios very different from specified treshold
-        console.log(entry.isIntersecting, entry.intersectionRatio);
         if (Math.abs(entry.intersectionRatio - OBSERVER_THRESHOLD) > 0.3) {
             continue;
         }
@@ -28,11 +27,27 @@ const appInfosObserver = new IntersectionObserver(entries => {
         // Don't use isIntersecting property.
         // On Edge, it is true if ratio > 0, without considering the threshold
         if (entry.intersectionRatio >= OBSERVER_THRESHOLD) {
-            entryScreenshot.classList.add('visible');
-        } else {
-            entryScreenshot.classList.remove('visible');
+            // This item has became visible
+            onNewVisibleItem(entryScreenshot);
         }
     }
 }, { threshold: [OBSERVER_THRESHOLD] });
+
+let prevVisibleItem;
+
+/**
+ * Call this function to trigger the screenshot change.
+ * Set the previous screenshot as invisible only when there's a new visible one,
+ * to avoid the risk of having no screenshot inside the phone mockup.
+ * 
+ * @param {HTMLElement} newVisibleItem The new item marked as visible
+ */
+function onNewVisibleItem(newVisibleItem) {
+    if (prevVisibleItem) {
+        prevVisibleItem.classList.remove('visible');
+    }
+    newVisibleItem.classList.add('visible');
+    prevVisibleItem = newVisibleItem;
+}
 
 appInfos.forEach(e => appInfosObserver.observe(e));
