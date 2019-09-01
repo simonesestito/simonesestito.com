daae95f3b782eacb791e542105d29acdaca2411f
Simone Sestito <simone@simonesestito.com>
2019-09-01 19:45:11 +0200

    Handle offline connection in email form

--- src/js/app.js
+++ src/js/app.js
@@ -10,9 +10,22 @@ import smoothscroll from 'smoothscroll-polyfill';
 // Smooth-scroll polyfill for Safari
 smoothscroll.polyfill();
 
+// WebP support accessible from CSS
 supportsWebP
     .then(supported => supported ? 'webp' : 'no-webp')
     .then(clazz => document.body.classList.add(clazz));
+
+// Online/offline detection in CSS
+function updateConnectionStatus() {
+    if (navigator.onLine) {
+        document.body.classList.remove('offline');
+    } else {
+        document.body.classList.add('offline');
+    }
+}
+window.addEventListener('online', updateConnectionStatus);
+window.addEventListener('offline', updateConnectionStatus);
+updateConnectionStatus();
 
 import './sections';
 import './chips';
