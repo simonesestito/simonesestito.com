c8f8b40ebd258194442f4dca0fbdaab4b69eb33e
Simone Sestito <simone@simonesestito.com>
2019-09-09 10:56:22 +0200

    Add Google Tag Manager

--- src/js/app.js
+++ src/js/app.js
@@ -62,6 +62,22 @@ document.addEventListener('cookiesaccepted', () => {
     gtag('config', 'UA-80600108-4', {
         'anonymize_ip': true
     });
+
+    // Google Tag Manager
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MFQT3SW');
 });
 
 import './sections';
